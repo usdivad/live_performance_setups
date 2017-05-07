d2be59eeeadfdcdc2302587b1b05aedcf6c43376
David Su <david.d.su@gmail.com>
2017-05-08 00:11:17 +0900

    initial files

---
+++ palmetto/src/palmetto.js
@@ -0,0 +1,11 @@
+(function() {
+    // var canvas = document.getElementById("canvas");
+    // var imageData = canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.height);
+    // var tracker = new HT.Tracker();
+    // var candidate =  tracker.detect(imageData);
+   
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMeida || navigator.mozGetUserMedia;
+    if (navigator.getUserMedia) {
+        
+    }
+});
