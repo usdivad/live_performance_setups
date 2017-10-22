ce65fae79d451e06897b832a829e3b64e546df89
David Su <david.d.su@gmail.com>
2017-10-22 19:24:32 -0400

    start spd viz; get midi->osc working

---
+++ viz/spd/bridge.js
@@ -0,0 +1,34 @@
+// From p5js-osc by Gene Kogan https://github.com/genekogan/p5js-osc
+
+var osc = require('node-osc');
+var io = require('socket.io')(8081);
+
+
+var oscServer, oscClient;
+
+var isConnected = false;
+
+console.log("asdf");
+
+io.sockets.on('connection', function (socket) {
+    console.log('connection');
+    socket.on("config", function (obj) {
+        isConnected = true;
+        oscServer = new osc.Server(obj.server.port, obj.server.host);
+        oscClient = new osc.Client(obj.client.host, obj.client.port);
+        oscClient.send('/status', socket.sessionId + ' connected');
+        oscServer.on('message', function(msg, rinfo) {
+            socket.emit("message", msg);
+        });
+        socket.emit("connected", 1);
+    });
+    socket.on("message", function (obj) {
+        oscClient.send.apply(oscClient, obj);
+    });
+    socket.on('disconnect', function(){
+        if (isConnected) {
+            oscServer.kill();
+            oscClient.kill();
+        }
+    });
+});
