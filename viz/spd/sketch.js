4bde81585771a313aedd5b4dbe770e4ee4aa639f
David Su <david.d.su@gmail.com>
2017-10-22 19:35:02 -0400

    draw the spd outline

--- viz/spd/sketch.js
+++ viz/spd/sketch.js
@@ -1,27 +1,57 @@
 // Adapted from p5js-osc/p5-basic sketch: https://github.com/genekogan/p5js-osc/tree/master/p5-basic
 
-var x, y;
-console.log("as");
+// var x, y;
+var numDivisionsX = 3;
+var numDivisionsY = 5;
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(800, 600);
     setupOsc(9001, 3334);
 }
 
 function draw() {
-    background(0, 0, 255);
-    fill(0, 255, 0);
-    ellipse(x, y, 100, 100);
-    fill(0);
-    text("I'm p5.js", x-25, y);
+    background(0, 0, 0);
+
+    // fill(0, 255, 0);
+    // ellipse(x, y, 100, 100);
+    // fill(0);
+    // text("I'm p5.js", x-25, y);
+
+    // Draw the SPD
+    stroke(255, 0, 0);
+    for (var i=1; i<numDivisionsY; i++) {
+        if (i == 2 || i == 4) {
+            continue;
+        }
+
+        var lineY = i * (height/numDivisionsY);
+        line(0, lineY, width, lineY);
+    }
+
+    for (var i=1; i<numDivisionsY; i++) {
+        var lineX = i * (width/numDivisionsX);
+        line(lineX, 0, lineX, height);
+    }    
+
+    // line(0, 100, width, 100);
+    
+
+    // fill(255, 0, 0);
+
 }
+
 
 function receiveOsc(address, value) {
     console.log("received OSC: " + address + ", " + value);
     
-    if (address == '/test') {
-        x = value[0];
-        y = value[1];
+    // if (address == '/test') {
+    //     x = value[0];
+    //     y = value[1];
+    // }
+
+    if (address == "/spd/in") {
+        var note = value[0];
+        var vel = value[1];
     }
 }
 
